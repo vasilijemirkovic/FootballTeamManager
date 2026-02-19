4a4f0e685c3a0362ca83b14f1d7b88ccff19614c
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-02-19 18:19:42 +0100

    Structure Stats added. It shows the number of goals, assists, yellow and red cards of the player.

--- player.h
+++ player.h
@@ -11,25 +11,42 @@ using namespace std;
 enum class Position { Goalkeeper, Defense, Midfield, Striker };
 const vector<string> positionNames{ "Goalkeeper", "Defense", "Midfield", "Striker" };
 
+
+struct Stats {
+    int goals = 0;
+    int assists = 0;
+    int yellowCards = 0;
+    int redCards = 0;
+};
+
 class Player
 {
 private:
     string name;
     int transferFee;
     Position position;
+    Stats stats;
 
 public:
     Player(const string& name, const int& transferFee, const Position& position = Position::Goalkeeper);
 
+    //getters
     const string& getName() const;
     int getTransferFee() const;
+    Position getPosition() const;
+    const Stats& getStats() const;
 
+    // update stats
+    void addGoal();
+    void addAssist();
+    void addYellowCard();
+    void addRedCard();
+
+    int getRating() const;
+    
     bool operator==(const Player& otherPlayer) const;
 
     friend ostream& operator<<(ostream& os, const Player& player);
-
-    // Bonus points:
-    Position getPosition() const;
 };
 
 #endif // PLAYER_H
