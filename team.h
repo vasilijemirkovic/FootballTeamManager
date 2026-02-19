ad54d4a0bc7536fe92e64f73bc29df6cbf167333
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-02-19 17:19:44 +0100

    Libraries cleaned. Usable comments added.

--- team.h
+++ team.h
@@ -2,6 +2,9 @@
 #define TEAM_H
 
 #include "player.h"
+#include <vector>
+#include <string>
+#include <ostream>
 
 class Team
 {
@@ -10,13 +13,18 @@ private:
     vector<Player> squad;
 
 public:
-    Team(const string& teamName, const vector<Player>& squad = vector<Player>());
-    vector<Player> signPlayers(const vector<Player>&);
-    friend ostream& operator<<(ostream&, const Team&);
+    Team(const string& teamName, const vector<Player>& squad = {});
+
+    vector<Player> signPlayers(const vector<Player>& squad);
+
+    friend ostream& operator<<(ostream& os, const Team& team);
 
     // Bonus points:
+
+    // Returns lineup as string: defense-midfield-striker
     string lineup() const;
 
+    // Release players by position; returns released players
     vector<Player> releasePlayers(const vector<Position>&);
 };
 
