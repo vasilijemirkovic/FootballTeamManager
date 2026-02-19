a2d5f042fa8630afea7715ee83c52516a39987a6
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-02-19 17:37:02 +0100

    Exception message separated.

--- player.cpp
+++ player.cpp
@@ -1,23 +1,36 @@
 #include "player.h"
 #include <stdexcept>
 
-Player::Player(const string& name, const int& transferAmount, const Position& position)
-    : name{ name }, transferAmount{ transferAmount }, position{ position }
+Player::Player(const string& name, const int& transferFee, const Position& position)
+    : name{ name }, transferFee{ transferFee }, position{ position }
 {
-    if (name.empty() || transferAmount < 10 || transferAmount > 500)
-        throw runtime_error("Error");
+    if (name.empty()){
+        throw runtime_error("Player name is missing!");
+    }
+
+    if (transferFee < 10 || transferFee > 500){
+        throw runtime_error("Transfer fee must be between 10 and 500 Mio Euros!");
+    }
 }
 
-bool Player::operator==(const Player& p) const
-{
-    return (name == p.name);
+const string& Player::getName() const {
+    return this->name;
 }
 
-ostream& operator<<(ostream& os, const Player& p)
+int Player::getTransferFee() const {
+    return this->transferFee;
+}
+
+
+bool Player::operator==(const Player& otherPlayer) const{
+    return (name == otherPlayer.name);
+}
+
+ostream& operator<<(ostream& os, const Player& player)
 {
-    return os << "[" << p.name << ", "
-              << p.transferAmount << " Mio Euro, "
-              << positionNames.at(static_cast<int>(p.position)) << "]";
+    return os << "[" << player.name << ", "
+              << player.transferFee << " Mio Euro, "
+              << positionNames.at(static_cast<int>(player.position)) << "]";
 }
 
 // Bonus points:
