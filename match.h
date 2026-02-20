6ecf0ffc3c2e02c5bd8a134bc18a2b19f08d6d37
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-02-20 23:11:39 +0100

    Initialization of the match class

---
+++ match.h
@@ -0,0 +1,34 @@
+#ifndef MATCH_H
+#define MATCH_H
+
+#include "team.h"
+#include <string>
+
+using namespace std;
+
+class Match{
+
+private:
+
+    Team& home;
+    Team& away;
+
+    int homeGoals = 0;
+    int awayGoals = 0;
+
+    bool played = false;
+
+    void simulateGoals(Team& scoringTeam, int goalCount);
+
+
+public:
+
+    Match(Team& home, Team& away);
+
+    void play();
+
+    string getScore() const;
+
+};
+
+#endif //MATCH_H
